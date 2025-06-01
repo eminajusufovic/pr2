f2bf73d517296f34d3a9928fb0960972dddaa3ce
Emina <154613302+eminajusufovic@users.noreply.github.com>
2025-06-01 15:17:12 +0200

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -4,10 +4,14 @@ amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
 cd /home/ec2-user
-rm -rf ISO-projekat2
-git clone https://github.com/eminajusufovic/ISO-projekat2.git
-cd ISO-projekat2
-sudo -u ec2-user docker-compose up -d
+rm -rf pr2
+git clone https://github.com/eminajusufovic/pr2.git
+chown -R ec2-user:ec2-user pr2
+
+cd pr2
+sudo -u ec2-user /usr/local/bin/docker-compose up -d
